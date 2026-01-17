81217f149624cdd9bc4555df49248d54cb5a75b9
Joseph Kinsey <kinzoe66@gmail.com>
2026-01-16 16:47:33 -1000

    Add filter system and align logic with doctrine

--- public/logic.js
+++ public/logic.js
@@ -1,7 +1,1 @@
-import { updateState } from "./state.js";
-
-export function runLogic() {
-  const state = updateState();
-  console.log("Logic executed:", state);
-  return state;
-}
+<PASTE LOGIC.JS CONTENT HERE>
